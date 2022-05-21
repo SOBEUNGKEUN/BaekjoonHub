1ce24b301bada0418fc6d9d92a66b209481439f2
소병권 <102494027+SOBEUNGKEUN@users.noreply.github.com>
2022-05-21 13:59:10 +0900

    [Bronze III] Title: 최소, 최대, Time: 508 ms, Memory: 149432 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -0,0 +1,12 @@
+count = int(input())
+number = list(map(int, input().split()))
+max = number[0]
+min = number[0]
+
+for i in number[1:]:
+    if i > max:
+        max = i
+    elif i < min:
+        min = i
+
+print(min,max)
